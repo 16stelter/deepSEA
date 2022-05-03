05b624c996696c2f296bd24b94b94a22f9ba4196
16stelter <6stelter@informatik.uni-hamburg.de>
2022-05-03 14:19:55 +0200

    Small fix

--- data_reader/data_reader/reader.py
+++ data_reader/data_reader/reader.py
@@ -1,5 +1,5 @@
 import rclpy
-from sensor_msgs.msg import Imu
+from sensor_msgs.msg import Imu, JointState
 from bitbots_msgs.msg import FootPressure, FloatStamped
 from message_filters import TimeSynchronizer, Subscriber
 from rclpy.node import Node
@@ -13,17 +13,18 @@ class Reader(Node):
         self.pressure_right_sub = Subscriber(self, FootPressure, "/foot_pressure_right/raw", qos_profile=1)
         self.lhall_sub = Subscriber(self, FloatStamped, "/hall/left", qos_profile=1)
         self.rhall_sub = Subscriber(self, FloatStamped, "/hall/right", qos_profile=1)
-        # self.motor_sub = self.create_subscription(Imu, "/imu/data", self.imu_cb, 1) TODO
-        # self.target_sub = self.create_subscription(Imu, "/imu/data", self.imu_cb, 1)
+        self.joint_sub = Subscriber(self, JointState, "/joint_states", qos_profile=1)
 
         self.ts = TimeSynchronizer([self.imu_sub,
                                     self.pressure_right_sub,
                                     self.pressure_right_sub,
                                     self.lhall_sub,
-                                    self.rhall_sub], 10)
+                                    self.rhall_sub,
+                                    self.joint_sub], 10)
         self.ts.registerCallback(self.syncCb)
 
-    def syncCb(self, msg):
+    def sync_cb(self, imu_msg, pressure_r_msg, pressure_l_msg, hall_l_msg, hall_r_msg, joint_msg):
+
         return
 
 
