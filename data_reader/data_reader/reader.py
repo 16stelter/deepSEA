972440c48ac846e640d5bd28711a66f307346ca7
16stelter <6stelter@informatik.uni-hamburg.de>
2022-06-01 16:35:17 +0200

    Fix data reader

--- data_reader/data_reader/reader.py
+++ data_reader/data_reader/reader.py
@@ -4,6 +4,7 @@ from bitbots_msgs.msg import FootPressure, FloatStamped
 from message_filters import TimeSynchronizer, Subscriber
 from rclpy.node import Node
 import pandas as pd
+import copy
 
 
 class Reader(Node):
@@ -15,7 +16,7 @@ class Reader(Node):
                         'imu', 'l_pressure', 'r_pressure']
         self.df = pd.DataFrame(columns=data_columns)
 
-        self.hist = [[0] * 10] * 10
+        self.hist = [[0 for i in range(10)] for i in range(13)]
 
         self.imu_sub = Subscriber(self, Imu, "/imu/data", qos_profile=1)
         self.pressure_left_sub = Subscriber(self, FootPressure, "/foot_pressure_left/raw", qos_profile=1)
@@ -23,35 +24,34 @@ class Reader(Node):
         self.lhall_sub = Subscriber(self, FloatStamped, "/hall/left", qos_profile=1)
         self.rhall_sub = Subscriber(self, FloatStamped, "/hall/right", qos_profile=1)
         self.joint_sub = Subscriber(self, JointState, "/joint_states", qos_profile=1)
-        self.ts = TimeSynchronizer([self.imu_sub,
-                                    self.pressure_right_sub,
-                                    self.pressure_right_sub,
-                                    self.lhall_sub,
+        self.ts = TimeSynchronizer([
                                     self.rhall_sub,
-                                    self.joint_sub], 10)
+                                    ], 10)
         self.ts.registerCallback(self.sync_cb)
 
-    def sync_cb(self, imu_msg, pressure_r_msg, pressure_l_msg, hall_l_msg, hall_r_msg, joint_msg):
-        for i in range(len(joint_msg.name)):
-            if joint_msg.name[i] == 'LKnee':
-                self.hist[0].append(joint_msg.position[i])
-                self.hist[2].append(joint_msg.velocity[i])
-                self.hist[4].append(joint_msg.current[i])
-            elif joint_msg.name[i] == 'Rknee':
-                self.hist[5].append(joint_msg.position[i])
-                self.hist[7].append(joint_msg.velocity[i])
-                self.hist[9].append(joint_msg.current[i])
-
-        self.hist[1].append(hall_l_msg.value)
+    def sync_cb(self, hall_r_msg):
+        # for i in range(len(joint_msg.name)):
+        #     if joint_msg.name[i] == 'LKnee':
+        #         self.hist[0].append(joint_msg.position[i])
+        #         self.hist[2].append(joint_msg.velocity[i])
+        #         self.hist[4].append(joint_msg.current[i])
+        #     elif joint_msg.name[i] == 'Rknee':
+        #         self.hist[5].append(joint_msg.position[i])
+        #         self.hist[7].append(joint_msg.velocity[i])
+        #         self.hist[9].append(joint_msg.current[i])
+        #
+        # self.hist[1].append(hall_l_msg.value)
         self.hist[6].append(hall_r_msg.value)
+        print(hall_r_msg)
         for i in self.hist:
-            i.pop(0)
-        data = self.hist
-        data.append(imu_msg)
-        data.append(pressure_l_msg)
-        data.append(pressure_r_msg)
-        self.df.append(data)
-        self.df.to_csv('dataset.csv')
+            if len(i) > 10:
+                i.pop(0)
+        data = copy.deepcopy(self.hist)
+        # data.append(imu_msg)
+        # data.append(pressure_l_msg)
+        # data.append(pressure_r_msg)
+        row = pd.Series(data, index=self.df.columns)
+        self.df = self.df.append(row, ignore_index=True)
 
 
 def main(args=None):
@@ -60,6 +60,7 @@ def main(args=None):
     try:
         rclpy.spin(node)
     except KeyboardInterrupt:
+        node.df.to_csv('dataset.csv')
         pass
     node.destroy_node()
     rclpy.shutdown()
