87a782905b33067aceba92c35c3e1a2d887dbe4c
16stelter <6stelter@informatik.uni-hamburg.de>
2022-08-02 12:40:23 +0200

    Add script to clean up data

---
+++ data_reader/scripts/data_cleaner.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import ast
+import math
+
+def quaternion_to_euler_angle(x, y, z, w):
+    ysqr = y * y
+
+    t0 = +2.0 * (w * x + y * z)
+    t1 = +1.0 - 2.0 * (x * x + ysqr)
+    X = math.degrees(math.atan2(t0, t1))
+
+    t2 = +2.0 * (w * y - z * x)
+    t2 = +1.0 if t2 > +1.0 else t2
+    t2 = -1.0 if t2 < -1.0 else t2
+    Y = math.degrees(math.asin(t2))
+
+    t3 = +2.0 * (w * z + x * y)
+    t4 = +1.0 - 2.0 * (ysqr + z * z)
+    Z = math.degrees(math.atan2(t3, t4))
+
+    return X, Y, Z
+
+
+df = pd.read_csv("pid_batt_d0.csv")
+droplist = []
+print(df)
+for index, row in df.iterrows():
+    a = ast.literal_eval(df.iloc[index, 12])
+    b = ast.literal_eval(df.iloc[index, 13])
+    if sum(a) + sum(b) < 15.0:
+        print("Pressure is " +  str(sum(a) + sum(b)) + "Dropping row.")
+        droplist.append(index)
+    imu = ast.literal_eval(df.iloc[index, 11])
+    e = list(quaternion_to_euler_angle(imu[0], imu[1], imu[2], imu[3]))
+    e.extend(imu[4:])
+    df.at[index, "imu"] = e 
+df = df.drop(droplist)      
+df.to_csv("pid_batt_d0c.csv")
+print(df)
