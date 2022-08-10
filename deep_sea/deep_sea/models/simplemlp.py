0f4bd00014e9afe2ff1280893c7f80a70f33a9b6
16stelter <6stelter@informatik.uni-hamburg.de>
2022-08-10 16:14:12 +0200

    Adds dropout layers

--- deep_sea/deep_sea/models/simplemlp.py
+++ deep_sea/deep_sea/models/simplemlp.py
@@ -1,18 +1,21 @@
 import torch.nn as nn
 import torch.nn.functional as F
 
+
 class SimpleMlp(nn.Module):
     def __init__(self, input_shape):
         super().__init__()
+        self.dropout = nn.Dropout(0.2)
         self.fc1 = nn.Linear(input_shape, 64)
         self.fc2 = nn.Linear(64, 64)
         self.fc3 = nn.Linear(64, 64)
         self.out = nn.Linear(64, 1)
 
     def forward(self, x):
-        x = F.leaky_relu(self.fc1(x))
-        x = F.leaky_relu(self.fc2(x))
-        x = F.leaky_relu(self.fc3(x))
+        x = self.dropout(x)
+        x = self.dropout(F.leaky_relu(self.fc1(x)))
+        x = self.dropout(F.leaky_relu(self.fc2(x)))
+        x = self.dropout(F.leaky_relu(self.fc3(x)))
         y = F.tanh(self.out(x))
 
         return y
