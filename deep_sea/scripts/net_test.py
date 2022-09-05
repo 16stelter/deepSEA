6c5772647ae136ebb8fcdd9d22ddbcbe90d302ea
16stelter <6stelter@informatik.uni-hamburg.de>
2022-09-05 11:28:38 +0200

    more net test

--- deep_sea/scripts/net_test.py
+++ deep_sea/scripts/net_test.py
@@ -10,20 +10,22 @@ from sensor_msgs.msg import Imu, JointState
 from bitbots_msgs.msg import FootPressure, FloatStamped, JointCommand
 from torch.utils.data import DataLoader
 from tqdm import tqdm
+from std_msgs.msg import Float64MultiArray
 
 
 class NetTest(Node):
     def __init__(self, input_size):
         super().__init__("nettest")
         self.model = SimpleMlp(input_size)
-        checkpoint = torch.load("checkpoints/old/free_SimpleMlp_53.pt")
+        checkpoint = torch.load("checkpoints/nopos_novel_SimpleMlp_19.pt")
         self.model.eval()
         self.model.load_state_dict(checkpoint)
 
+        self.pub_sample = self.create_publisher(Float64MultiArray, "/debug/sample", 1)
         self.pub_target = self.create_publisher(FloatStamped, "/debug/target", 1)
         self.pub_prediction = self.create_publisher(FloatStamped, "/debug/prediction", 1)
 
-        ds = SeaDataset("data/dfreecn.csv", hist_len=1, vel=True, eff=True)
+        ds = SeaDataset("data/dfreecn.csv", hist_len=0, vel=False, eff=False, hall=False)
 
         self.loader = DataLoader(ds, batch_size=1, shuffle=False)
 
@@ -31,17 +33,17 @@ class NetTest(Node):
 
     def test_loop(self):
         for i, (x, y) in enumerate(tqdm(self.loader)):
-            #x, y = next(iter(self.loader))
-            if i % 2 == 1:
-                print(x)
-                print(y)
-                print("--")
+            if i % 2 == 0:
+                sample_msg = Float64MultiArray()
+                sample_msg.data = [i * math.pi for i in x.tolist()[0]]
+                self.pub_sample.publish(sample_msg)
+
                 target_msg = FloatStamped()
                 target_msg.value = y.item() * math.pi
                 self.pub_target.publish(target_msg)
 
                 pred_msg = FloatStamped()
-                pred_msg.value = self.model.forward(x).item() * math.pi
+                pred_msg.value = (self.model.forward(x).item() * math.pi)
                 self.pub_prediction.publish(pred_msg)
 
 
@@ -51,7 +53,7 @@ class NetTest(Node):
 
 def main(args=None):
     rclpy.init(args=None)
-    node = NetTest(6)
+    node = NetTest(1)
     rclpy.spin(node)
     node.destroy_node()
     rclpy.shutdown()
